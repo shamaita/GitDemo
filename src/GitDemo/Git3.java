58ed8973dd059844cb5fcda06edebac59fa1d5c3
shamaita <palshamaita1998@gmail.com>
2023-08-19 13:45:52 +0530

    Asian boy part

---
+++ src/GitDemo/Git3.java
@@ -0,0 +1,14 @@
+package GitDemo;
+
+public class Git3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 //Git
+        System.out.println("Hi welcome to git");
+        
+        System.out.println("This is my senond line");
+        System.out.println("Hi asia");
+	}
+
+}
