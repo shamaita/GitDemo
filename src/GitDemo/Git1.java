19fef91148a1ff2b2e87c1b3e10beada3dada058
shamaita <palshamaita1998@gmail.com>
2023-08-19 13:14:47 +0530

    new branch

---
+++ src/GitDemo/Git1.java
@@ -0,0 +1,15 @@
+package GitDemo;
+
+public class Git1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 //Git
+        System.out.println("Hi welcome to git");
+        
+        System.out.println("This is my senond line");
+        System.out.println("Hi asia");
+
+	}
+
+}
---
+++ src/GitDemo/Git2.java
@@ -0,0 +1,15 @@
+package GitDemo;
+
+public class Git2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				 //Git
+		        System.out.println("Hi welcome to git");
+		        
+		        System.out.println("This is my senond line");
+		        System.out.println("Hi asia");
+	}
+
+}
